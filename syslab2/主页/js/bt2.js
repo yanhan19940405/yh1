226ce8c06277fc6efec511ac26a79a8329434ea7
yanhan19940405 <1257157255@qq.com>
2015-04-09 19:32:05 +0800

    页面全部上传

---
+++ syslab2/主页/js/bt2.js
@@ -0,0 +1,47 @@
+function MM_over(mmObj) {
+	var mSubObj = mmObj.getElementsByTagName("div")[0];
+	mSubObj.style.display = "block";
+	mSubObj.style.backgroundColor = ":#C0C0C0";
+	
+	
+}
+function MM_out(mmObj) {
+	var mSubObj = mmObj.getElementsByTagName("div")[0];
+	mSubObj.style.display = "none";
+	
+}
+function showA(obj)
+{
+obj.src="images/2.png";
+}
+function showB(obj)
+{
+obj.src="images/3.png"  
+}
+
+function showc(obj)
+{
+obj.src="images/4.png";
+}
+function showd(obj)
+{
+obj.src="images/5.png"  
+}
+
+function showe(obj)
+{
+obj.src="images/6.png";
+}
+function showf(obj)
+{
+obj.src="images/7.png"  
+}
+
+function showg(obj)
+{
+obj.src="images/8.png";
+}
+function showh(obj)
+{
+obj.src="images/9.png"  
+}
